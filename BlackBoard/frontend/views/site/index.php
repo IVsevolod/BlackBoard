fc21c68080a005b494fce1e427d7408599f2d951
IVsevolod <v.ivanov@zavento.com>
2016-07-11 16:39:56 +0300

    Init

---
+++ BlackBoard/frontend/views/site/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Доска объявлений';
+?>
+<div class="site-index">
+    <div class="body-content">
+        <div class="row">
+
+        </div>
+    </div>
+</div>
