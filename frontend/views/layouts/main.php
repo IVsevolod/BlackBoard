ad5e24d3672aca52b351d2ca5634bfce85cf65dc
IVsevolod <v.ivanov@zavento.com>
2016-07-18 11:36:50 +0300

    Счетчик

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -77,14 +77,16 @@ AppAsset::register($this);
         <div class="pull-right">
             <!--LiveInternet counter--><script type="text/javascript"><!--
                 document.write("<a href='//www.liveinternet.ru/click' "+
-                    "target=_blank><img src='//counter.yadro.ru/hit?t45.1;r"+
+                    "target=_blank><img src='//counter.yadro.ru/hit?t16.6;r"+
                     escape(document.referrer)+((typeof(screen)=="undefined")?"":
                     ";s"+screen.width+"*"+screen.height+"*"+(screen.colorDepth?
                         screen.colorDepth:screen.pixelDepth))+";u"+escape(document.URL)+
                     ";"+Math.random()+
-                    "' alt='' title='LiveInternet' "+
-                    "border='0' width='31' height='31'><\/a>")
+                    "' alt='' title='LiveInternet: показано число просмотров за 24"+
+                    " часа, посетителей за 24 часа и за сегодня' "+
+                    "border='0' width='88' height='31'><\/a>")
                 //--></script><!--/LiveInternet-->
+
             <!-- Yandex.Metrika counter -->
             <script type="text/javascript">
                 (function (d, w, c) {
